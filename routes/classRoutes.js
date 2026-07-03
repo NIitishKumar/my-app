284a9ce0e41a87730b8b5b7ae90129897af876b0
NIitishKumar <nitishkunar.ng746@gmail.com>
2026-07-03 20:20:01 +0530

    change in schema

--- routes/classRoutes.js
+++ routes/classRoutes.js
@@ -33,6 +33,6 @@ router.post("/:id/students", addStudentToClass);
 // DELETE /api/classes/:id/students/:studentId - Remove student from class
 router.delete("/:id/students/:studentId", removeStudentFromClass);
 
-route.put("/:classId/remove-student", removeStudentFromClassById);
+router.put("/:classId/remove-student", removeStudentFromClassById);
 
 module.exports = router;
